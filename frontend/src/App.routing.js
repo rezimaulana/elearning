7ba7f48f39dddca56a1bfeb7f0615d1a8755500a
rezimaulana <rsazrm@gmail.com>
2022-12-29 16:19:21 +0700

    Created Toast

--- frontend/src/App.routing.js
+++ frontend/src/App.routing.js
@@ -9,10 +9,10 @@ export const router = createBrowserRouter([
 //   },
   {
     path: "login",
-    element: < Login />,
+    element: <Login/>,
   },
   {
     path: "register",
-    element: < Register />,
+    element: <Register/>,
   },
 ]);
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,42 +1,80 @@
-import { useNavigate } from "react-router-dom"
 import { Button } from "../components/Button"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import axios from "../lib/axios"
+import { ToastContainer, toast } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+    
     const navigate = useNavigate()
+
+    const LOGIN_URL = "/login"
+
+    const [email, setEmail] = useState("");
+
+    const [password, setPassword] = useState("");
+    
     const register = () => {
         navigate('/register')
     }
+
+    const emailHandle = (e) => {
+        setEmail(e.target.value);
+    }
+    
+    const passwordHandle = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const login = () => {
+        axios
+            .post(LOGIN_URL, {
+            email: email,
+            password: password,
+        })
+            .then((result) => {
+            localStorage.setItem("data", JSON.stringify(result.data));
+            navigate("/");
+        })
+            .catch((err) => {
+            toast.error(err.response.data.message);
+        })
+    }
+
     return(
-        <div className="container-fluid bg-light">
-            <div className="d-flex justify-content-center align-items-center vh-100">
-                <div className="card">
-                    <div className="card-header">
-                        <div className="row justify-content-center align-items-center">
-                            <div className="col-md-7">
-                                E-Learning
-                            </div>
-                            <div className="col-md-5 mt-2 mb-1">
-                                <Button type={"button"} className={"btn btn-primary"} id={"btnRegister"} name={"btnRegister"} label={"Register"} onClick={register}/>
+        <>
+            <div className="container-fluid bg-light">
+                <div className="d-flex justify-content-center align-items-center vh-100">
+                    <div className="card">
+                        <div className="card-header">
+                            <div className="row justify-content-center align-items-center">
+                                <div className="col-md-7">
+                                    E-Learning
+                                </div>
+                                <div className="col-md-5 mt-2 mb-1">
+                                    <Button type={"button"} className={"btn btn-primary"} id={"btnRegister"} 
+                                        name={"btnRegister"} label={"Register"} onClick={register}/>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="card-body">
-                        <form className="justify-content-center">
-                            <label htmlFor="email" className="form-label">Email</label>
-                            <input type="email" className="form-control" id="email" name="email" disabled />
-                            <label htmlFor="password" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="password" name="password" disabled />
-                            <p className="card-text mt-3">Klik tombol untuk login.</p>
-                            <div className="btn-group">
-                                <button type="button" className="btn btn-primary" id="btnLoginAdmin">Admin</button>
-                                <button type="button" className="btn btn-secondary" id="btnLoginInstructor">Guru</button>
-                                <button type="button" className="btn btn-info" id="btnLoginStudent">Siswa</button>
-                            </div>
-                        </form>
+                        <div className="card-body">
+                            <form className="justify-content-center">
+                                <label htmlFor="email" className="form-label">Email</label>
+                                <input type="email" className="form-control" id="email" name="email" onChange={emailHandle}/>
+                                <label htmlFor="password" className="form-label">Password</label>
+                                <input type="password" className="form-control" id="password" name="password" onChange={passwordHandle}/>
+                                <div className="btn-group mt-3">
+                                    <Button type={"button"} className={"btn btn-primary"} id={"btnLoginSubmit"}
+                                        name={"btnLoginSubmit"} label={"Login"} onClick={login}/>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <ToastContainer/>
+        </>
     )
 }
 
